2b33d1a40a7070c2be33395473b48f404ff1fc8e
Hyobeen-Park <amanda0109@naver.com>
2024-04-03 04:23:46 +0900

    feat: 회원가입 기능 구현

--- app/src/main/java/com/sopt/now/LoginActivity.kt
+++ app/src/main/java/com/sopt/now/LoginActivity.kt
@@ -1,20 +1,42 @@
 package com.sopt.now
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.sopt.now.databinding.ActivityLoginBinding
+import com.sopt.now.databinding.ActivitySignupBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var resultLauncher: ActivityResultLauncher<Intent>
     private lateinit var binding : ActivityLoginBinding
+    private var id = ""
+    private var pw = ""
+    private var nickname = ""
+    private var mbti = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // 회원가입 버튼 클릭이벤트
         binding.btLoginSignup.setOnClickListener {
-            var intent = Intent(this, SignupActivity::class.java)
-            startActivity(intent)
+            val intent = Intent(this, SignupActivity::class.java)
+            resultLauncher.launch(intent)
+        }
+
+        // 회원가입 정보 받아오기
+        resultLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                id = result.data?.getStringExtra("id") ?: ""
+                pw = result.data?.getStringExtra("pw") ?: ""
+                nickname = result.data?.getStringExtra("nickname") ?: ""
+                mbti = result.data?.getStringExtra("mbti") ?: ""
+            }
         }
     }
 }
--- app/src/main/java/com/sopt/now/SignupActivity.kt
+++ app/src/main/java/com/sopt/now/SignupActivity.kt
@@ -1,6 +1,8 @@
 package com.sopt.now
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.sopt.now.databinding.ActivitySignupBinding
 
@@ -10,5 +12,41 @@ class SignupActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivitySignupBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // Toast 출력될 문자열
+        var message : String?
+
+        // 회원가입 버튼 클릭이벤트
+        binding.btSignupSignup.setOnClickListener {
+            // 입력 조건 + 공백 문자만 있지 않은지 확인
+            if(binding.etSignupId.text.isBlank() || binding.etSignupId.length() < 6 || binding.etSignupId.length() > 10) {
+                message = "ID는 6자 이상, 10자 이하로 입력해주세요"
+            } else if(binding.etSignupPw.text.isBlank() || binding.etSignupPw.length() < 8 || binding.etSignupPw.length() > 12) {
+                message = "비밀번호는 8자 이상, 12자 이하로 입력해주세요"
+            } else if(binding.etSignupNickname.text.isBlank()) {
+                message = "닉네임을 입력해주세요"
+            } else if(binding.etSignupMbti.text.isBlank()) {
+                message = "MBTI를 입력해주세요"
+            } else {
+                message = null
+            }
+
+            if(message == null) {
+                // 입력 조건 모두 충족했을 경우 -> 회원가입 성공
+                Toast.makeText(this, "회원가입에 성공했습니다!", Toast.LENGTH_SHORT).show()
+
+                // intent에 정보 담아서 로그인 화면으로 전달
+                val intent = Intent(this, LoginActivity::class.java)
+                intent.putExtra("id", binding.etSignupId.text.toString())
+                intent.putExtra("pw", binding.etSignupPw.text.toString())
+                intent.putExtra("nickname", binding.etSignupNickname.text.toString())
+                intent.putExtra("mbti", binding.etSignupMbti.text.toString())
+                setResult(RESULT_OK, intent)
+                finish()
+            } else {
+                // 입력 조건을 충족하지 못한 경우 -> 토스트 메세지
+                Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
