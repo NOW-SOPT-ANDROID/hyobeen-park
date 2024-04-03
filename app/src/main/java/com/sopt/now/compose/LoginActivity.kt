01180b7d4e754105aaa774dc191dfa3ca03828a8
Hyobeen-Park <amanda0109@naver.com>
2024-04-04 00:27:30 +0900

    feat: #2 로그인 ui 구현

---
+++ app/src/main/java/com/sopt/now/compose/LoginActivity.kt
@@ -0,0 +1,134 @@
+package com.sopt.now.compose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight.Companion.Bold
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.sopt.now.compose.ui.theme.NOWSOPTAndroidTheme
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
+
+class LoginActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NOWSOPTAndroidTheme {
+                Surface (
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Login() {
+    var id by remember { mutableStateOf("") }
+    var pw by remember { mutableStateOf("") }
+    Column (
+        modifier = Modifier
+            .padding(horizontal = 70.dp)
+            .fillMaxWidth()
+    ) {
+        Spacer(modifier = Modifier.height(70.dp))
+        Text(
+            text = "Welcome To SOPT",
+            fontSize = 30.sp,
+            fontWeight = Bold,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(150.dp))
+        Text(
+            text = "ID",
+            fontSize = 20.sp,
+            fontWeight = Bold,
+            textAlign = TextAlign.Start,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        TextField(
+            value = id,
+            onValueChange = {
+                id = it
+            },
+            modifier = Modifier
+                .fillMaxWidth(),
+            label = {Text("아이디를 입력해주세요")}
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        Text(
+            text = "비밀번호",
+            fontSize = 20.sp,
+            fontWeight = Bold,
+            textAlign = TextAlign.Start,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        TextField(
+            value = pw,
+            onValueChange = {
+                pw = it
+            },
+            modifier = Modifier
+                .fillMaxWidth(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            label = {Text("비밀번호를 입력해주세요")}
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        Button(
+            onClick = { /*TODO*/ },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFF209672)
+            ),
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "로그인")
+        }
+        Button(
+            onClick = { /*TODO*/ },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Transparent
+            ),
+            modifier = Modifier.align(Alignment.End)
+            ) {
+            Text(text = "회원가입하기", color = Color.Black)
+        }
+    }
+}
+
+@Preview (showBackground = true)
+@Composable
+fun LoginPreview() {
+    Login()
+}
