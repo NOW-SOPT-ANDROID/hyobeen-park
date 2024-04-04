b36bc6f9127f3038023e22a68cbfe8ba4f0204cd
Hyobeen-Park <amanda0109@naver.com>
2024-04-04 10:43:36 +0900

    feat: #2 회원가입 기능 구현

--- app/src/main/java/com/sopt/now/compose/SignupActivity.kt
+++ app/src/main/java/com/sopt/now/compose/SignupActivity.kt
@@ -1,6 +1,10 @@
 package com.sopt.now.compose
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Nickname
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
@@ -16,6 +20,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -24,6 +29,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
@@ -50,6 +56,7 @@ class SignupActivity : ComponentActivity() {
 
 @Composable
 fun Signup() {
+    val context = LocalContext.current
     var id by remember { mutableStateOf("") }
     var pw by remember { mutableStateOf("") }
     var nickname by remember { mutableStateOf("") }
@@ -142,7 +149,27 @@ fun Signup() {
         )
         Spacer(modifier = Modifier.height(20.dp))
         Button(
-            onClick = { /*TODO*/ },
+            onClick = {
+                when {
+                    id.isBlank() || id.length < 6 || id.length > 10 ->
+                        Toast.makeText(context, "아이디는 6글자 이상, 10글자 이하로 입력해주세요", Toast.LENGTH_SHORT).show()
+                    pw.isBlank() || pw.length < 8 || pw.length > 12 ->
+                        Toast.makeText(context, "비밀번호는 8글자 이상, 12글자 이하로 입력해주세요", Toast.LENGTH_SHORT).show()
+                    nickname.isBlank() ->
+                        Toast.makeText(context, "닉네임을 입력해주세요", Toast.LENGTH_SHORT).show()
+                    mbti.isBlank() ->
+                        Toast.makeText(context, "mbti를 입력해주세요", Toast.LENGTH_SHORT).show()
+                    else -> {
+                        Toast.makeText(context, "회원가입에 성공했습니다.", Toast.LENGTH_SHORT).show()
+                        val intent = Intent(context, LoginActivity::class.java)
+                        intent.putExtra("id", id)
+                        intent.putExtra("pw", pw)
+                        intent.putExtra("nickname", nickname)
+                        intent.putExtra("mbti", mbti)
+                        context.startActivity(intent)
+                    }
+                }
+            },
             colors = ButtonDefaults.buttonColors(
                 containerColor = Color(0xFF209672)
             ),
