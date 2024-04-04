63a1fcc67034052855a838974e786e0d88237d4f
Hyobeen-Park <amanda0109@naver.com>
2024-04-04 10:12:20 +0900

    feat: #2 회원가입 ui 구현

--- app/src/main/java/com/sopt/now/compose/SignupActivity.kt
+++ app/src/main/java/com/sopt/now/compose/SignupActivity.kt
@@ -1,6 +1,5 @@
 package com.sopt.now.compose
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,10 +9,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -27,17 +24,15 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.font.FontWeight.Companion.Bold
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.sopt.now.compose.ui.theme.NOWSOPTAndroidTheme
-import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
-class LoginActivity : ComponentActivity() {
+class SignupActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -46,7 +41,7 @@ class LoginActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Login()
+                    Signup()
                 }
             }
         }
@@ -54,10 +49,11 @@ class LoginActivity : ComponentActivity() {
 }
 
 @Composable
-fun Login() {
-    val context = LocalContext.current
+fun Signup() {
     var id by remember { mutableStateOf("") }
     var pw by remember { mutableStateOf("") }
+    var nickname by remember { mutableStateOf("") }
+    var mbti by remember { mutableStateOf("") }
     Column (
         modifier = Modifier
             .padding(horizontal = 70.dp)
@@ -65,17 +61,17 @@ fun Login() {
     ) {
         Spacer(modifier = Modifier.height(70.dp))
         Text(
-            text = "Welcome To SOPT",
+            text = "SIGN UP",
             fontSize = 30.sp,
-            fontWeight = Bold,
+            fontWeight = FontWeight.Bold,
             textAlign = TextAlign.Center,
             modifier = Modifier.fillMaxWidth()
         )
-        Spacer(modifier = Modifier.height(150.dp))
+        Spacer(modifier = Modifier.height(50.dp))
         Text(
             text = "ID",
             fontSize = 20.sp,
-            fontWeight = Bold,
+            fontWeight = FontWeight.Bold,
             textAlign = TextAlign.Start,
             modifier = Modifier.fillMaxWidth()
         )
@@ -87,13 +83,13 @@ fun Login() {
             },
             modifier = Modifier
                 .fillMaxWidth(),
-            label = {Text("아이디를 입력해주세요")}
+            label = { Text("아이디를 입력해주세요") }
         )
         Spacer(modifier = Modifier.height(20.dp))
         Text(
             text = "비밀번호",
             fontSize = 20.sp,
-            fontWeight = Bold,
+            fontWeight = FontWeight.Bold,
             textAlign = TextAlign.Start,
             modifier = Modifier.fillMaxWidth()
         )
@@ -106,7 +102,43 @@ fun Login() {
             modifier = Modifier
                 .fillMaxWidth(),
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-            label = {Text("비밀번호를 입력해주세요")}
+            label = { Text("비밀번호를 입력해주세요") }
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        Text(
+            text = "닉네임",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Start,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        TextField(
+            value = nickname,
+            onValueChange = {
+                nickname = it
+            },
+            modifier = Modifier
+                .fillMaxWidth(),
+            label = { Text("닉네임을 입력해주세요") }
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        Text(
+            text = "MBTI",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Start,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        TextField(
+            value = mbti,
+            onValueChange = {
+                mbti = it
+            },
+            modifier = Modifier
+                .fillMaxWidth(),
+            label = { Text( "MBTI를 입력해주세요") }
         )
         Spacer(modifier = Modifier.height(20.dp))
         Button(
@@ -116,25 +148,13 @@ fun Login() {
             ),
             modifier = Modifier.fillMaxWidth()
         ) {
-            Text(text = "로그인")
-        }
-        Button(
-            onClick = {
-                val intent = Intent(context, SignupActivity::class.java)
-                context.startActivity(intent)
-            },
-            colors = ButtonDefaults.buttonColors(
-                containerColor = Color.Transparent
-            ),
-            modifier = Modifier.align(Alignment.End)
-            ) {
-            Text(text = "회원가입하기", color = Color.Black)
+            Text(text = "회원가입하기")
         }
     }
 }
 
-@Preview (showBackground = true)
+@Preview(showBackground = true)
 @Composable
-fun LoginPreview() {
-    Login()
+fun SignupPreview() {
+    Signup()
 }
