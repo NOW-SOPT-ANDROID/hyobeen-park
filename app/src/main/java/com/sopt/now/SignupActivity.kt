369768f942a3f8b29cd9ce6ec2aeff9b72fb2b13
Hyobeen-Park <amanda0109@naver.com>
2024-04-16 20:50:17 +0900

    mod: #1 회원가입 화면 함수화

--- app/src/main/java/com/sopt/now/SignupActivity.kt
+++ app/src/main/java/com/sopt/now/SignupActivity.kt
@@ -13,47 +13,54 @@ class SignupActivity : AppCompatActivity() {
         binding = ActivitySignupBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Toast 출력될 문자열
-        var message : String?
+        initSignUpBtnClickListener()
 
-        // 회원가입 정보 저장
-        val id = binding.etSignupId.text
-        val pw = binding.etSignupPw.text
-        val nickname = binding.etSignupNickname.text
-        val mbti = binding.etSignupMbti.text
+    }
 
+    private fun initSignUpBtnClickListener() {
         // 회원가입 버튼 클릭이벤트
         binding.btSignupSignup.setOnClickListener {
-            // 입력 조건 + 공백 문자만 있지 않은지 확인
-            when {
-                id.isBlank() || id.length < 6 || id.length > 10 ->
-                    message = "ID는 6자 이상, 10자 이하로 입력해주세요"
-                pw.isBlank() || pw.length < 8 || pw.length > 12 ->
-                    message = "비밀번호는 8자 이상, 12자 이하로 입력해주세요"
-                nickname.isBlank() ->
-                    message = "닉네임을 입력해주세요"
-                mbti.isBlank() ->
-                    message = "MBTI를 입력해주세요"
-                else -> message = null
-            }
-
-            if(message == null) {
-                // 입력 조건 모두 충족했을 경우 -> 회원가입 성공
-                Toast.makeText(this, "회원가입에 성공했습니다!", Toast.LENGTH_SHORT).show()
-
-                // intent에 정보 담아서 로그인 화면으로 전달
-                Intent(this, LoginActivity::class.java).apply {
-                    putExtra("id", id.toString())
-                    putExtra("pw", pw.toString())
-                    putExtra("nickname", nickname.toString())
-                    putExtra("mbti", mbti.toString())
-                    setResult(RESULT_OK, this)
-                    finish()
-                }
-            } else {
-                // 입력 조건을 충족하지 못한 경우 -> 토스트 메세지
-                Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+            // 회원가입 정보 저장
+            with(binding) {
+                val id = etSignupId.text.toString()
+                val pw = etSignupPw.text.toString()
+                val nickname = etSignupNickname.text.toString()
+                val mbti = etSignupMbti.text.toString()
+                checkInputValid(id, pw, nickname, mbti)
             }
         }
+    }
+
+    private fun checkInputValid(id : String, pw : String, nickname : String, mbti : String) {
+        // 입력 조건 + 공백 문자만 있지 않은지 확인
+        when {
+            id.isBlank() || id.length < 6 || id.length > 10 ->
+                showToastMessage("ID는 6자 이상, 10자 이하로 입력해주세요")
+            pw.isBlank() || pw.length < 8 || pw.length > 12 ->
+                showToastMessage("비밀번호는 8자 이상, 12자 이하로 입력해주세요")
+            nickname.isBlank() ->
+                showToastMessage("닉네임을 입력해주세요")
+            mbti.isBlank() ->
+                showToastMessage("MBTI를 입력해주세요")
+            else -> successSignUp(id, pw, nickname, mbti)
+        }
+    }
+
+    private fun successSignUp(id : String, pw : String, nickname : String, mbti : String) {
+        showToastMessage("회원가입에 성공했습니다!")
+
+        Intent(this, LoginActivity::class.java).apply {
+            putExtra("id", id)
+            putExtra("pw", pw)
+            putExtra("nickname", nickname)
+            putExtra("mbti", mbti)
+            setResult(RESULT_OK, this)
+            finish()
+        }
+    }
+
+
+    private fun showToastMessage(message : String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
