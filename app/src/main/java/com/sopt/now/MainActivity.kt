f6d0d5fdf0a4d595c4734dda7880cf4944b90eb0
Hyobeen-Park <amanda0109@naver.com>
2024-04-17 21:51:43 +0900

    mod: #5 main -> mypage 코드 이동

--- app/src/main/java/com/sopt/now/MainActivity.kt
+++ app/src/main/java/com/sopt/now/MainActivity.kt
@@ -3,9 +3,6 @@ package com.sopt.now
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.sopt.now.databinding.ActivityMainBinding
-import com.sopt.now.LoginActivity.Companion.ID
-import com.sopt.now.LoginActivity.Companion.MBTI
-import com.sopt.now.LoginActivity.Companion.NICKNAME
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding : ActivityMainBinding
@@ -14,17 +11,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        initTextViews()
 
     }
 
-    private fun initTextViews() {
-        intent.apply {
-            with(binding) {
-                tvMainNickname.setText(getStringExtra(NICKNAME))
-                tvMainIdContent.setText(getStringExtra(ID))
-                tvMainMbtiContent.setText(getStringExtra(MBTI))
-            }
-        }
-    }
 }
--- app/src/main/java/com/sopt/now/MypageFragment.kt
+++ app/src/main/java/com/sopt/now/MypageFragment.kt
@@ -6,6 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.sopt.now.databinding.FragmentMypageBinding
+import com.sopt.now.LoginActivity.Companion.ID
+import com.sopt.now.LoginActivity.Companion.MBTI
+import com.sopt.now.LoginActivity.Companion.NICKNAME
 
 class MypageFragment : Fragment() {
     private var _binding : FragmentMypageBinding? = null
@@ -23,11 +26,21 @@ class MypageFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        initTextViews()
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun initTextViews() {
+        activity?.intent?.apply {
+            with(binding) {
+                tvMainNickname.setText(getStringExtra(NICKNAME))
+                tvMainIdContent.setText(getStringExtra(ID))
+                tvMainMbtiContent.setText(getStringExtra(MBTI))
+            }
+        }
     }
 }
