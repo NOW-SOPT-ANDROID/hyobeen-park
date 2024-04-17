5f450b40e3179dbb9c2c43da765ebb0169f56ada
Hyobeen-Park <amanda0109@naver.com>
2024-04-17 21:41:48 +0900

    feat: #5 바텀 네비게이션 연결

--- app/src/main/java/com/sopt/now/HomeActivity.kt
+++ app/src/main/java/com/sopt/now/HomeActivity.kt
@@ -2,6 +2,7 @@ package com.sopt.now
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.sopt.now.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
@@ -11,5 +12,42 @@ class HomeActivity : AppCompatActivity() {
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val currentFragment = supportFragmentManager.findFragmentById(binding.fcvHome.id)
+        if(currentFragment == null) {
+            supportFragmentManager.beginTransaction()
+                .add(binding.fcvHome.id, HomeFragment())
+                .commit()
+        }
+
+        clickBottomNavigation()
+    }
+
+    private fun clickBottomNavigation() {
+        binding.bnvHome.setOnItemSelectedListener{
+            when (it.itemId) {
+                R.id.menu_home-> {
+                    replaceFragment(HomeFragment())
+                    true
+                }
+
+                R.id.menu_search-> {
+                    replaceFragment(SearchFragment())
+                    true
+                }
+
+                R.id.menu_mypage-> {
+                    replaceFragment(MypageFragment())
+                    true
+                }
+
+                else -> false
+            }
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fcv_home, fragment)
+            .commit()
     }
 }
