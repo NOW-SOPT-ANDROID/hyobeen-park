1de9d99b86e718487541ceddefd1ecb863a6a276
Hyobeen-Park <amanda0109@naver.com>
2024-05-02 22:18:16 +0900

    mod: #6 null 체크 let 함수 ?:로 수정

--- app/src/main/java/com/sopt/now/compose/HomeScreen.kt
+++ app/src/main/java/com/sopt/now/compose/HomeScreen.kt
@@ -7,27 +7,23 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
-fun HomeScreen(nickname: String?, mbti: String?){
+fun HomeScreen(nickname: String?, mbti: String?) {
     val homeViewModel = HomeViewModel()
     val friendsList = homeViewModel.getFriendsData()
 
     LazyColumn {
         item {
-            nickname?.let {
-                mbti?.let { it1 ->
-                    UserItem(
-                        painter = R.drawable.ic_launcher_foreground,
-                        contentDescription = "User Profile Image",
-                        imageSize = 70.dp,
-                        nickname = it,
-                        mbti = it1,
-                        fontSize = 24.sp
-                    )
-                }
-            }
+            UserItem(
+                painter = R.drawable.ic_launcher_foreground,
+                contentDescription = "User Profile Image",
+                imageSize = 70.dp,
+                nickname = nickname ?: "닉네임",
+                mbti = mbti ?: "mbti",
+                fontSize = 24.sp
+            )
         }
 
-        items(friendsList) {friendsList ->
+        items(friendsList) { friendsList ->
             UserItem(
                 painter = friendsList.profileImage,
                 contentDescription = "Friends profile image",
