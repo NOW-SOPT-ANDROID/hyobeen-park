ddec22a3341a4d2da98a24b656007e5cf023fa8e
Hyobeen-Park <amanda0109@naver.com>
2024-05-03 03:48:15 +0900

    feat: #9 mypage 회원 정보 구현

--- app/src/main/java/com/sopt/now/Mypage/MypageFragment.kt
+++ app/src/main/java/com/sopt/now/Mypage/MypageFragment.kt
@@ -5,12 +5,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.sopt.now.R
 import com.sopt.now.User.User
+import com.sopt.now.data.DTO.response.ResponseUserInfoDto
 import com.sopt.now.data.Key.USER
+import com.sopt.now.data.service.ServicePool
 import com.sopt.now.databinding.FragmentMypageBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MypageFragment : Fragment() {
+    private val authService by lazy { ServicePool.authService }
     private var _binding: FragmentMypageBinding? = null
     private val binding: FragmentMypageBinding
         get() = requireNotNull(_binding) { }
@@ -26,7 +34,7 @@ class MypageFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initTextViews()
+        getUserInfo(getUserId())
     }
 
     override fun onDestroyView() {
@@ -34,18 +42,38 @@ class MypageFragment : Fragment() {
         _binding = null
     }
 
-    private fun initTextViews() {
-        activity?.intent?.apply {
-            with(binding) {
-                val user = if (Build.VERSION.SDK_INT >= 33) {
-                    getParcelableExtra(USER, User::class.java)
-                } else {
-                    getParcelableExtra(USER)
-                }
-                tvMypageNickname.setText(user?.nickname)
-                tvMypageIdContent.setText(user?.id)
-                tvMypagePhoneContent.setText(user?.phone)
-            }
+    private fun initTextViews(id: String, nickname: String, phone: String) {
+        with(binding) {
+            tvMypageNickname.setText(nickname)
+            tvMypageIdContent.setText(id)
+            tvMypagePhoneContent.setText(phone)
         }
+    }
+
+    private fun getUserId(): String {
+        return activity?.intent?.getStringExtra("userId") ?: "0"
+    }
+
+    private fun getUserInfo(userId: String) {
+        authService.getUserInfo(userId).enqueue(object : Callback<ResponseUserInfoDto> {
+            override fun onResponse(
+                call: Call<ResponseUserInfoDto>,
+                response: Response<ResponseUserInfoDto>
+            ) {
+                if (response.isSuccessful) {
+                    val data: ResponseUserInfoDto? = response.body()
+                    data?.data?.apply {
+                        initTextViews(authenticationId, nickname, phone)
+                    }
+                } else {
+                    val error = response.message()
+                    Toast.makeText(context, error, Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun onFailure(call: Call<ResponseUserInfoDto>, t: Throwable) {
+                Toast.makeText(context, "서버 통신 에러", Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 }
