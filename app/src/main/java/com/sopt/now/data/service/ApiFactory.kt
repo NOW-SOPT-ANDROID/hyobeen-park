d19864ee65fa65713a0871310411dc66db1bae7e
Hyobeen-Park <amanda0109@naver.com>
2024-05-03 07:12:15 +0900

    add: #9 user list api url 추가

--- app/src/main/java/com/sopt/now/data/service/ApiFactory.kt
+++ app/src/main/java/com/sopt/now/data/service/ApiFactory.kt
@@ -1,24 +1,25 @@
 package com.sopt.now.data.service
 
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
-import com.sopt.now.BuildConfig
+import com.sopt.now.BuildConfig.AUTH_BASE_URL
+import com.sopt.now.BuildConfig.USER_API_URL
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
 import retrofit2.Retrofit
 
 object ApiFactory {
-    private const val BASE_URL: String = BuildConfig.AUTH_BASE_URL
+    fun retrofit(url: String) : Retrofit =
+            Retrofit.Builder()
+                .baseUrl(url)
+                .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
+                .build()
 
-    val retrofit: Retrofit by lazy {
-        Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
-            .build()
-    }
 
-    inline fun <reified T> create(): T = retrofit.create(T::class.java)
+
+    inline fun <reified T, B> create(url: B): T = retrofit(url.toString()).create(T::class.java)
 }
 
 object ServicePool {
-    val authService = ApiFactory.create<AuthService>()
+    val authService = ApiFactory.create<AuthService, String>(AUTH_BASE_URL)
+    val userService = ApiFactory.create<UserService, String>(USER_API_URL)
 }
