0701c445a63b3d8e48bd47b5213886996d0b66aa
Hyobeen-Park <amanda0109@naver.com>
2024-05-03 07:18:33 +0900

    feat: #9 회원 리스트 api DTO 구현

---
+++ app/src/main/java/com/sopt/now/data/DTO/response/ResponseUserListDto.kt
@@ -0,0 +1,32 @@
+package com.sopt.now.data.DTO.response
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ResponseUserListDto(
+    @SerialName("page")
+    val page: Int,
+    @SerialName("per_page")
+    val per_page: Int,
+    @SerialName("total")
+    val total: Int,
+    @SerialName("total_pages")
+    val total_pages: Int,
+    @SerialName("data")
+    val data: List<ResponseUserListDataDto>
+)
+
+@Serializable
+data class ResponseUserListDataDto(
+    @SerialName("id")
+    val id: Int,
+    @SerialName("email")
+    val email: String,
+    @SerialName("first_name")
+    val first_name: String,
+    @SerialName("last_name")
+    val last_name: String,
+    @SerialName("avatar")
+    val avatar: String,
+)
---
+++ app/src/main/java/com/sopt/now/data/service/UserService.kt
@@ -0,0 +1,13 @@
+package com.sopt.now.data.service
+
+import com.sopt.now.data.DTO.response.ResponseUserListDto
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface UserService {
+    @GET("api/users")
+    fun getUserList(
+        @Query("page") page: Int,
+    ): Call<ResponseUserListDto>
+}
