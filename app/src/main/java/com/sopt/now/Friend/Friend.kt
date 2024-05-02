b9d336fc11cff4b0039fe3dd34ed7cccbdf678f7
Hyobeen-Park <amanda0109@naver.com>
2024-05-03 08:19:41 +0900

    feat: #9 회원 리스트 조회 기능 구현

--- app/src/main/java/com/sopt/now/Friend/Friend.kt
+++ app/src/main/java/com/sopt/now/Friend/Friend.kt
@@ -1,11 +1,18 @@
 package com.sopt.now.Friend
 
-import androidx.annotation.DrawableRes
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class Friend(
+    @SerialName("id")
     val id: Int,
+    @SerialName("email")
     val email: String,
+    @SerialName("first_name")
     val first_name: String,
+    @SerialName("last_name")
     val last_name: String,
+    @SerialName("avatar")
     val avatar: String,
 )
--- app/src/main/java/com/sopt/now/Home/HomeFragment.kt
+++ app/src/main/java/com/sopt/now/Home/HomeFragment.kt
@@ -10,12 +10,14 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.ConcatAdapter
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.sopt.now.Friend.Friend
 import com.sopt.now.Friend.FriendAdapter
 import com.sopt.now.R
 import com.sopt.now.User.User
 import com.sopt.now.User.UserAdapter
 import com.sopt.now.data.DTO.response.ResponseUserInfoDataDto
 import com.sopt.now.data.DTO.response.ResponseUserInfoDto
+import com.sopt.now.data.DTO.response.ResponseUserListDto
 import com.sopt.now.data.Key.USER
 import com.sopt.now.data.service.ServicePool
 import com.sopt.now.databinding.FragmentHomeBinding
@@ -25,12 +27,14 @@ import retrofit2.Response
 
 class HomeFragment : Fragment() {
     private val authService by lazy { ServicePool.authService }
+    private val userService by lazy { ServicePool.userService }
     private var _binding: FragmentHomeBinding? = null
     private val binding: FragmentHomeBinding
         get() = requireNotNull(_binding) { }
 
     private val homeViewModel = HomeViewModel()
     private var user: User? = null
+    private var friendList: List<Friend> ?= null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -45,6 +49,8 @@ class HomeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         initUserData()
+        getUserList()
+
     }
 
     override fun onDestroyView() {
@@ -60,7 +66,8 @@ class HomeFragment : Fragment() {
 
     private fun initFriendRecyclerView() {
         val userAdapter = UserAdapter().apply { user?.let { setUserList(it) } }
-        val friendAdapter = FriendAdapter().apply { setFriendList(homeViewModel.getFriendInfo()) }
+        val friendAdapter = FriendAdapter().apply { friendList?.let { setFriendList(it) } }
+
 
         binding.rvHomeFriends.run {
             adapter = ConcatAdapter(userAdapter, friendAdapter)
@@ -94,4 +101,27 @@ class HomeFragment : Fragment() {
             }
         })
     }
+
+    private fun getUserList() {
+        userService.getUserList(2).enqueue(object : Callback<ResponseUserListDto> {
+            override fun onResponse(
+                call: Call<ResponseUserListDto>,
+                response: Response<ResponseUserListDto>
+            ) {
+                if(response.isSuccessful) {
+                    val data : ResponseUserListDto? = response.body()
+                    friendList = data?.data
+                    initFriendRecyclerView()
+                } else {
+                    val error = response.message()
+                    Toast.makeText(context, error, Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun onFailure(call: Call<ResponseUserListDto>, t: Throwable) {
+                Toast.makeText(context, "서버 통신 에러", Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
 }
